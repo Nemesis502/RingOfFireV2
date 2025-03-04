3f46a6eb88b09f2ee26fc3ff872bc40d90e4320c
Nemesis502 <klawes-bastian@t-online.de>
2025-03-04 20:40:18 +0100

    added firestore to array / download

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { Firestore, addDoc, collection, doc, onSnapshot, setDoc } from '@angular/fire/firestore';
 import { inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 
 
@@ -15,34 +16,49 @@ import { inject } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
+
+  firestore: Firestore = inject(Firestore);
+
   pickCardAnimation = false;
   currentCard: string = "";
   game!: Game;
+  // unsubGame;
 
-  unsubGame;
-
-  constructor(private firestore: Firestore, public dialog: MatDialog) {
-    this.unsubGame = onSnapshot(this.getGameRef(), (list) => {
-      list.forEach(element => {
-        console.log(this.setGameObject(element.data()));
-
-      })
-    })
-
+  constructor(private route: ActivatedRoute, public dialog: MatDialog) {
+    // this.unsubGame = ;
   }
 
   ngOnInit(): void {
+    this.initGame()
+  }
+
+  // ngonDestroy() {
+  //   this.unsubGame();
+  // }
+
+  initGame() {
     this.newGame();
+    this.route.params.subscribe((params) => {
+      console.log(params['id']);
+      return onSnapshot(this.getGameRef(params['id']), (game) => {
+        let uploadedGame = this.setGameObject(game.data())
+        this.game.currentPlayer = uploadedGame.currentPlayer;
+        this.game.playedCards = uploadedGame.playedCard;
+        this.game.players = uploadedGame.players;
+        this.game.stack = uploadedGame.stack;
+      })
+    })
     this.addNewGame();
   }
+
+
 
   newGame() {
     this.game = new Game();
   }
 
   async addNewGame() {
-   let gameinfo = await addDoc(this.getGameRef(), this.game.toJson())
-   console.log(gameinfo);
+    // await addDoc(this.getGameRef(), this.game.toJson())
   }
 
   takecard() {
@@ -78,14 +94,17 @@ export class GameComponent {
     });
   }
 
-  getGameRef() {
-    return collection(this.firestore, 'games');
+  getGameRef(params: string) {
+    return doc(collection(this.firestore, 'games'), params);
   }
 
   setGameObject(obj: any) {
-      return {
-        game: obj.game || "",
-      }
+    return {
+      currentPlayer: obj.currentPlayer || 0,
+      playedCard: obj.playedCards || [],
+      players: obj.players || [],
+      stack: obj.stack || [],
+    }
   }
 }
 
